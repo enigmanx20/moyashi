a3e2fe9bf60eca54c1386da4a064fbc01c79b41d
Satoshi Funayama <akchan.acts@gmail.com>
2014-07-06 15:45:00 +0900

    AnalysisController was created.
    MoyashiController module which is used to extend ApplicationController class was created.

---
+++ app/controllers/analysis_controller.rb
@@ -0,0 +1,59 @@
+class AnalysisController < MoyashiAnalysisController
+
+  define_params :pca, {pcx: :integer, pcy: :integer, file1: :file, file2: :file}
+  
+  def pca
+  end
+end
+
+
+
+module MoyashiController
+
+  PARAMS_TYPES = %i[integer float string text file]
+
+
+
+  module ClassMethods
+    def define_params(name, params)
+      name += "_show"
+      define_method name do
+        raise 'Invalid parameter type' unless valid_params_type?(params)
+        @_moyashi_show_params = params
+      end
+    end
+
+
+
+    private
+      def valid_params_type?(params)
+        flg = true
+
+        params.values do |v|
+          unless PARAMS_TYPES.include?(v)
+            flg = false
+            break
+          end
+        end
+
+        flg
+      end
+  end
+
+
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+end
+
+
+
+class MoyashiAnalysisController < ApplicationController
+  include MoyashiController
+end
+
+
+
+
+
